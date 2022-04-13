a09a2f5738d8c831c45e46ca853696f18370cfac
vayvoo <platinmuz@gmail.com>
2022-04-13 16:06:32 +0500

    first commit

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,28 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-app
+    id="inspire"
+    :style="{ background: $vuetify.theme.themes.dark.background }"
+  >
+    <Sidebar />
+  </v-app>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+// import HelloWorld from '../components/HelloWorld';
+import Sidebar from '../components/Sidebar.vue';
+// import { Carousel3d, Slide } from 'vue-carousel-3d';
 
 export default {
-  name: 'HomeView',
+  data: () => ({
+    slides: [
+      { src: require('../../public/12.jpg'), text: 'Cheb Khaled' },
+      { src: require('../../public/02.jpeg'), text: 'Sooling' },
+      { src: require('../../public/03.png'), text: 'Chakira' },
+    ],
+  }),
   components: {
-    HelloWorld
-  }
-}
+    Sidebar,
+    // Carousel3d,
+  },
+};
 </script>
